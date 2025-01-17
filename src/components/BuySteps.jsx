d9cfe4ea1f869f6d16c7c65bd22b4da0900b7dbd
AmaliaRibeiro <amalita9421@gmail.com>
2025-01-17 19:22:54 -0300

    feat: add changes css

--- src/components/BuySteps.jsx
+++ src/components/BuySteps.jsx
@@ -5,33 +5,33 @@ const Steps = () => {
   const steps = [
     {
       number: "1",
-      title: "ELEGÍ EL PRODUCTO",
+      title: "DESCUBRÍ TU PRODUCTO IDEAL",
       description:
-        "Navegá en nuestro e-shop, contamos con una gran variedad de prendas, seleccioná los artículos que más te gusten, indicando el talle, color y cantidad que deseas adquirir.",
+        "Explorá nuestro e-shop de Amma, donde encontrarás una amplia variedad de calzados y bikinis exclusivos. Elegí tus artículos favoritos indicando talle, color y cantidad, ¡y disfrutá de un estilo único pensado para vos!",
     },
     {
       number: "2",
-      title: "HACÉ EL PEDIDO",
+      title: "CONFIRMÁ TU PEDIDO",
       description:
-        "En tu carrito vas a encontrar los productos que hayas elegido anteriormente, para confirmar la compra es necesario que finalices tu pedido, ingresando tus datos de pago y dirección de envío.",
+      "En tu carrito están tus productos seleccionados. Para completar la compra, ingresa tus datos de pago y dirección de envío. Si el producto está disponible, se enviará al día siguiente; si está pendiente, se enviará en un plazo de hasta 10 días hábiles.",
     },
     {
       number: "3",
-      title: "FORMA DE PAGO Y ENVÍO",
+      title: "PAGÁ Y RECIBÍ EN TIEMPO Y FORMA",
       description:
-        "Utilizamos los servicios de terceros para ofrecerte la máxima seguridad a la hora de realizar tu pago. Garantizamos la integridad y pronta entrega de la prenda gracias al servicio de logística.",
+        "En Amma trabajamos con servicios de pago seguros y confiables para garantizar la máxima tranquilidad en cada transacción. Nos aseguramos de que tus prendas lleguen a tiempo y en perfecto estado, gracias a un servicio de logística ágil y eficiente."
     },
     {
       number: "4",
-      title: "DISFRUTÁ TU PRENDA",
+      title: "DISFRUTÁ TU COMPRA EN AMMA",
       description:
-        "En los próximos 10 días hábiles de haber confirmado tu pago, la compra llegará a destino. Recibirás un código de seguimiento para que puedas conocer en todo momento el estado de tu envío.",
+        "Una vez confirmado tu pago, tu compra llegará a destino dentro de los próximos 10 días hábiles. Te proporcionaremos un código de seguimiento para que puedas verificar el estado de tu envío en cualquier momento."
     },
   ];
 
   return (
     <>
-    <p className={styles.stepTitle}> PASOS A SEGUIR PARA COMPRAR EN AMMA</p>
+    <p className={styles.stepTitle}> TUS PRIMEROS PASOS CON AMMA</p>
     <div className={styles.container}>
       {steps.map((step, index) => (
         <div key={index} className={styles.card}>

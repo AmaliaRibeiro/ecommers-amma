ec6dd8bef1555036f864501c9bec466a371a07fb
AmaliaRibeiro <amalita9421@gmail.com>
2025-01-29 15:44:21 -0300

    correccion de error

--- backend/src/routes/formRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { handleFormSubmission } = require("../controllers/formController");
-
-// Ruta para recibir el formulario
-router.post("/", handleFormSubmission);
-
-module.exports = router;

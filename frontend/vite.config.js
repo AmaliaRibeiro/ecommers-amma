acc20459ef37154922bfbf824e2e979e6abeb3ce
AmaliaRibeiro <amalita9421@gmail.com>
2025-02-05 10:07:20 -0300

    feat: add folders

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Esto asegura que las rutas sean relativas
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Asegura que el build se guarde en la carpeta correcta
+    }
 })
